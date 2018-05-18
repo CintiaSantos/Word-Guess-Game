fe9f1a2a1cc5029410ef461ab71aabe8b2fa8a97
Cintia Santos <cintiasantos@Cintias-MacBook-Pro.local>
2018-05-17 20:04:39 -0400

    Initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,49 @@
+    // this is our array of letters in the alphabet 
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+        var targetLetter;
+        var wins = 0;
+        var losses = 0;
+        var guessesLeft = 9;
+        var guessed = [];
+        
+        
+        function renderDivs() {
+            document.getElementById("wins").innerHTML = "Your wins so far: " + wins;
+            document.getElementById("losses").innerHTML = "Your losses so far: " + losses;
+            document.getElementById("guessesLeft").innerHTML = `You have ${guessesLeft} guesses left.`;
+            document.getElementById("guessed").innerHTML = "letters you have guessed: " + guessed.join(", ")
+        }
+        
+        renderDivs();
+        
+        function startGame() {
+            var numIndex = Math.floor(Math.random() * 27);
+            targetLetter = alphabet[numIndex];
+            console.log(targetLetter)
+        }
+        
+        startGame();
+        
+        document.onkeyup = function (e) {
+            var clientGuess = e.key
+        
+            if (clientGuess === targetLetter) {
+                alert("You win!")
+            } else {
+                if (guessesLeft < 1) {
+                    alert("You lose sucka.  Game over");
+                    losses++;
+                    guessesLeft = 9;
+                    guessed = [];
+                    startGame();
+                    renderDivs();
+                } else {
+                    guessesLeft--
+                    guessed.push(clientGuess)
+                    renderDivs();
+                    console.log("You didn't win yet")
+                }
+            }
+        }
